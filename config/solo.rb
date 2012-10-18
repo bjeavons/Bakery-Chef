284605ae27a7f67f69462a551411c11f65957262
Glenn Pratt <glennpratt@gmail.com>
2012-10-18 08:11:53 -0500

    Set Chef logging to :debug.

--- config/solo.rb
+++ config/solo.rb
@@ -3,6 +3,10 @@
 #
 #repo_root = "#{Dir.tmpdir}/chefsolo"
 
+# Extra debugging till it works consistently.
+log_level          :debug
+log_location       STDOUT
+
 base_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 cookbook_path ["#{base_path}/cookbooks", "#{base_path}/site-cookbooks"]
 
