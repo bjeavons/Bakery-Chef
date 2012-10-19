1caadd0c21b35ccbf34401c4c10425023bdd8a41
Glenn Pratt <glennpratt@gmail.com>
2012-10-18 23:36:20 -0500

    Fix step definition missing RSpec matcher.

--- tests/bakery/features/support/env.rb
+++ tests/bakery/features/support/env.rb
@@ -1,8 +1,10 @@
 puts "loading env.rb"
+
 require 'capybara'
 require 'capybara/dsl'
 require "capybara/cucumber"
 require 'capybara/poltergeist'
+begin require 'rspec/expectations'; rescue LoadError; require 'spec/expectations'; end
 
 Capybara.default_driver = :poltergeist
 
