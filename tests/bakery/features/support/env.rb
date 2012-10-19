7d02b9fee78ecd8e364b5fb22da349f5271387ee
Glenn Pratt <glennpratt@gmail.com>
2012-10-19 07:13:03 -0500

    Extend wait time to give it plenty of time for pages to load.

--- tests/bakery/features/support/env.rb
+++ tests/bakery/features/support/env.rb
@@ -8,4 +8,7 @@ begin require 'rspec/expectations'; rescue LoadError; require 'spec/expectations
 
 Capybara.default_driver = :poltergeist
 
+# Can be slow with empty caches, give it some time.
+Capybara.default_wait_time = 120
+
 #Usually, we would just use an environment variable for that (ENV['SOMETHING'])
