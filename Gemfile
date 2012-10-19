ace061cb6078cf1374dc43824fac7f4f7ac343c7
Glenn Pratt <glennpratt@gmail.com>
2012-10-18 19:02:16 -0500

    Change default test to mechanize, don't think we need javascript for Bakery often.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,4 @@
 source "http://rubygems.org"
 gem "capybara"
 gem "cucumber"
+gem "capybara-mechanize"
--- tests/bakery/features/support/env.rb
+++ tests/bakery/features/support/env.rb
@@ -2,7 +2,8 @@ puts "loading env.rb"
 require 'capybara'
 require 'capybara/dsl'
 require "capybara/cucumber"
+require 'capybara/mechanize/cucumber'
 
-Capybara.default_driver = :selenium
+Capybara.default_driver = :mechanize
 
 #Usually, we would just use an environment variable for that (ENV['SOMETHING'])
